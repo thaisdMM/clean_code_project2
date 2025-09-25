bfd5e4a67f36d3190e8b70db85eb00b6e44658d4
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-09-25 19:49:50 +0200

    feat(validators): create error_types, implementing error_types in error_halder and create user_creator_validador with pydantic

---
+++ src/validators/errors/error_handler.py
@@ -0,0 +1,24 @@
+from src.views.http_types.http_response import HttpResponse
+from src.validators.error_types.http_bad_request import HttpBadRequestError
+from src.validators.error_types.http_not_found import HttpNotFoundError
+from src.validators.error_types.http_unprocessable_entity import (
+    HttpUnprocessableEntityError,
+)
+
+
+def handler_errors(error: Exception) -> HttpResponse:
+    if isinstance(
+        error, (HttpNotFoundError, HttpBadRequestError, HttpUnprocessableEntityError)
+    ):
+        # chamar um log
+        # alertar alguem...
+
+        return HttpResponse(
+            status_code=error.status_code,
+            body={"errors": [{"title": error.name, "detail": error.message}]},
+        )
+
+    return HttpResponse(
+        status_code=500,
+        body={"errors": [{"title": "Server Error", "detail": str(error)}]},
+    )
---
+++ src/validators/user_creator_validator.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, constr, ValidationError
+from src.validators.error_types.http_unprocessable_entity import (
+    HttpUnprocessableEntityError,
+)
+
+
+def user_creator_validator(http_request: any) -> None:
+
+    # Esta classe é definida localmente para agrupar as regras de validação do Pydantic. Serve para:
+    #  Aumentar a legibilidade: Agrupar a lógica de validação e a definição de dados no mesmo lugar.
+    # Limitar a visibilidade: Evitar que a definição de dados "vaze" para outras partes do código.
+    class BodyData(BaseModel):
+        person_nam: constr(min_length=1)  # type: ignore
+        age: int
+        height: float
+
+    try:
+        BodyData(**http_request.body)
+
+    except ValidationError as error:
+        raise HttpUnprocessableEntityError(error.errors()) from error
