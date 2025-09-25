c7ec22129a0369d5b0d9b59eb74fb2fdd2792a3e
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-09-25 19:52:12 +0200

    feat(views): implementing user_creator_validator and error_handler

--- src/views/user_creator_view.py
+++ src/views/user_creator_view.py
@@ -1,4 +1,6 @@
 from src.controllers.interfaces.user_creator import UserCreatorInterface
+from src.validators.errors.error_handler import handler_errors
+from src.validators.user_creator_validator import user_creator_validator
 from .http_types.http_request import HttpRequest
 from .http_types.http_response import HttpResponse
 
@@ -9,11 +11,14 @@ class UserCreatorView:
 
     def handler_insert_new_user(self, request: HttpRequest) -> HttpResponse:
         try:
+            user_creator_validator(request)
             person_name = request.body["person_name"]
             age = request.body["age"]
             height = request.body["height"]
 
             response = self.__controller.insert_new_user(person_name, age, height)
+
             return HttpResponse(status_code=200, body=response)
+
         except Exception as exception:
-            return HttpResponse(body={"error": str(exception)}, status_code=500)
+            return handler_errors(exception)
